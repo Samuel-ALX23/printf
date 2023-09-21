af0cc88b86d257db461e5c198c7ed2655da4d396
Yodaa90 <myoda067@gmail.com>
2023-09-21 07:56:21 +0000

    print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -11,14 +11,12 @@ int print_binary(int j)
 	int i = 0;
 	int n[64];
 	int x;
-if (i >= 64)
-{
-	return (-1);
-}
+
 
 	if (j == 0)
 	{
 		_putshar('0');
+		return (0);
 	}
 
 	while (j > 0)
@@ -26,6 +24,10 @@ if (i >= 64)
 		n[i] = (j % 2);
 		j = (j / 2);
 		i++;
+	}
+	if (i >= 64)
+	{
+		return (-1);
 	}
 
 	for (x = i - 1; x >= 0; x--)
